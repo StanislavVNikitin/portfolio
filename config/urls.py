ff3c7b8eca4444601ed7ef52f5eea62495359856
stas <stasvnikitin@mail.ru>
2024-01-22 14:11:13 +0300

    Update urls

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('akabinet/', admin.site.urls),
     path('captcha/', include('captcha.urls')),
     path('', include("portfolio.urls")),
 ]
